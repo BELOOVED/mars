cb0c290be36d4d9495bc4c62cb67e15d2a5f3913
BELOOOVED <aycahanim@outlook.com>
2023-05-28 22:19:43 +0300

    modified:   m/view/modules/modals.php

--- m/view/modules/modals.php
+++ m/view/modules/modals.php
@@ -327,11 +327,11 @@
             </div>
         </div>
     </div>
-    <div class="ReactModalPortal" id="withdraw-modal">
+    <div class="ReactModalPortal modal" id="withdraw-modal">
     <div class="ReactModal__Overlay ReactModal__Overlay--after-open" aria-modal="true" style="position: fixed; inset: 0px; background-color: rgba(255, 255, 255, 0.75);">
         <div class="ReactModal__Content ReactModal__Content--after-open payment-modal " tabindex="-1">
         <div class="modal-header">
-            <i class="icon-arrow-left"></i>
+            <i class="icon-arrow-left" onclick="closemodal()"></i>
             <h2>PARA ÇEKİM</h2>
         </div>
         <div class="modal-content">
