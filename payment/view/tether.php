d778369a0a6fb0d386d94cc95fbc32b1256b9fc3
BELOOOVED <aycahanim@outlook.com>
2023-05-29 17:51:45 +0300

    modified:   payment/view/tether.php

--- payment/view/tether.php
+++ payment/view/tether.php
@@ -13,7 +13,7 @@ function getCoinPrice($coin_id, $vs_currency) {
   return $data[$coin_id][$vs_currency];
 }
 
-$lira_amount = $_GET['amount'];
+$lira_amount = $_POST['amount'];
 
 $coin_id = "tether";
 
